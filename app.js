4cf32f447762e53ce7399989bb0e875f77550936
samir shrestha <sht.samir12@gmail.com>
2023-09-26 18:07:33 +0545

    single blog and login added

--- app.js
+++ app.js
@@ -8,8 +8,19 @@ app.use(express.static("public/"));
 //using ejs
 app.set("view engine", "ejs");
 
+//home
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+//blog
+app.get("/blog", (req, res) => {
+  res.render("components/singleblog.ejs");
+});
+
+//login
+app.get("/login", (req, res) => {
+  res.render("components/form/login.ejs");
 });
 app.listen(8000, () => {
   console.log("Project is running at port 8000");
